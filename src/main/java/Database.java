6ab4ee1a7e20a8c5dc92f199373db4322f136e46
Sherie Snodderly <114764452+ssnodderly@users.noreply.github.com>
2023-08-04 16:44:54 -0500

    Updated with a different idea of how I want to go with this program.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,3 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// This class is to represent the data storage system.
 public class Database {
-    // This class is to represent the data storage system.
+    private Map<String, User> users;
+    private Map<String, TaskList> taskLists;
+
+    public Database() {
+        users = new HashMap<>();
+        taskLists = new HashMap<>();
+    }
+    // User operations
+    public User getUser(String username) {
+        return users.get(username);
+    }
+
+    public void addUser(String username, User user) {
+        users.put(username, user);
+    }
+
+    public boolean containsUser(String username) {
+        return users.containsKey(username);
+    }
+
+    // TaskList operations
+    public TaskList getTaskList(String username) {
+        return taskLists.get(username);
+    }
+
+    public void addTaskList(String username, TaskList taskList) {
+        taskLists.put(username, taskList);
+    }
 }
--- src/main/java/TaskApp.java
+++ src/main/java/TaskApp.java
@@ -1,5 +1,100 @@
+import java.util.Scanner;
 
+// This class serves as the main entry point to the application.
 public class TaskApp {
-    // This ties all of these components together.
 
+    private TaskManager taskManager;
+    private Scanner scanner;
+
+    public TaskApp() {
+        this.taskManager = new TaskManager();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    register();
+                    break;
+                case "2":
+                    login();
+                    break;
+                case "3":
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    private void register() {
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+        try {
+            taskManager.createUser(username, password);
+            System.out.println("User registered successfully!");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Username already in use.");
+        }
+    }
+
+    private void login() {
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        if (taskManager.authenticateUser(username, password)) {
+            System.out.println("Logged in successfully!");
+            manageTasks(username);
+        } else {
+            System.out.println("Invalid credentials.");
+        }
+    }
+
+    private void manageTasks(String username) {
+        while (true) {
+            System.out.println("1. Create task");
+            System.out.println("2. View tasks");
+            System.out.println("3. Edit task");
+            System.out.println("4. Delete task");
+            System.out.println("5. Log out");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    // Implement create task functionality here
+                    break;
+                case "2":
+                    // Implement view tasks functionality here
+                    break;
+                case "3":
+                    // Implement edit task functionality here
+                    break;
+                case "4":
+                    // Implement delete task functionality here
+                    break;
+                case "5":
+                    return;
+                default:
+                    System.out.println("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new TaskApp().start();
+    }
 }
